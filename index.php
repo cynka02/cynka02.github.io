fe0eec27d8e4f8928094f5409c3f06f037a1b7e6
cynka02 <33747300+cynka02@users.noreply.github.com>
2017-11-17 10:07:23 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>Clicker</title>
+<script src="scripts.js"></script>
+</head>
+<body onload="load()">
+	<div>Letters: <span id="ClickDiv">0.00</span></div><br />
+	<div>Letters per second: <span id="PerSecond">0.00</span></div><br />
+	<button onclick="LettersCountTracker()" id="ClickButton">+ 1</button><br />
+	<button onclick="LetterClickPlus()" id="LetterClickPlusButton">100 letters --> upgrade click</button><br />
+	<button onclick="Pen()" id="PenButton">20 letters --> +1.00 letters per second</button><br />
+	<button onclick="UpgradePenfunc()" id="UpgradePen">30 letters --> pens upgrade</button><br />
+	<button onclick="reset()">Reset</button>
+</body>
+</html>
