af9fa95206a150a5bb29974384b5e46280ccdd04
cynka02 <33747300+cynka02@users.noreply.github.com>
2021-12-29 20:42:33 +0100

    Update scripts.js

--- client/scripts.js
+++ client/scripts.js
@@ -1,62 +1,87 @@
 var socket = io();
-var letters = [];
 var inputfield = [];
 var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyząćęłńóśżźĄĆĘŁŃÓŚŻŹ1234567890-=!@#$%^&*()_+[]{};'" + '\:"|,./<>?';
-var characterscolors = ["Backspace", "Tab", "Enter", "CapsLock", "Shift", "Control", "Alt", "AltGraph", " "]
+var characterscodes = ["Backspace", "Tab", "CapsLock", "ShiftRight", "ControlRight", "AltRight", "AltLeft", "Space", "ControlLeft", "ShiftLeft", "Enter"];
+var Caps = 0;
+
 function load(){
 	var classkeys = document.getElementsByClassName("keys");
 	for (let i=0; i < classkeys.length; i++){
 		classkeys[i].style = "line-height: " + classkeys[0].clientHeight + "px;";
 	}
-	document.getElementById("Backspace").style = "line-height: " + classkeys[0].clientHeight + "px;";
-	document.getElementById("Tab").style = "line-height: " + classkeys[0].clientHeight + "px;";
-	document.getElementById("CapsLock").style = "line-height: " + classkeys[0].clientHeight + "px;";
-	document.getElementById("Backslash").style = "line-height: " + classkeys[0].clientHeight + "px;";
-	document.getElementById("Shift").style = "line-height: " + classkeys[0].clientHeight + "px;";
-	document.getElementById("Control").style = "line-height: " + classkeys[0].clientHeight + "px;";
-	document.getElementById("Alt").style = "line-height: " + classkeys[0].clientHeight + "px;";
-	document.getElementById("Space").style = "line-height: " + classkeys[0].clientHeight + "px;";
-	document.getElementById("AltGraph").style = "line-height: " + classkeys[0].clientHeight + "px;";
+	for (let i=0; i < characterscodes.length; i++){
+		document.getElementById(characterscodes[i]).style = "line-height: " + classkeys[0].clientHeight + "px;";
+	}
 }
-window.addEventListener('resize', load);
-document.addEventListener('keydown', (event) => {
-	var name = event.key;
-	var code = event.code;
-	if (characters.includes(name) == true || characterscolors.includes(name) == true || code == "Backslash")
-	{
-		if (name == " "){
-			document.getElementById("Space").style.backgroundColor = "red";
-			setTimeout(clearcolors, 400, "Space");
-		}
-		else if (code == "Backslash"){
-			document.getElementById("Backslash").style.backgroundColor = "red";
-			setTimeout(clearcolors, 400, "Backslash");
-		}
-		else{
-			document.getElementById(name).style.backgroundColor = "red";
-			setTimeout(clearcolors, 400, name);
+function changecaps(value){
+	var tempchar = characters.substring(26,52);
+	var tempchar2 = characters.substring(0,26);
+	if (value == 1){
+		for (let i=0; i < tempchar.length; i++){
+			document.getElementById(tempchar[i]).innerHTML = tempchar2[i];
 		}
 	}
+	else if (value == 0){
+		for (let i=0; i < tempchar.length; i++){
+			document.getElementById(tempchar[i]).innerHTML = tempchar[i];
+		}
+	}
+}
+
+window.addEventListener('resize', load);
+document.addEventListener('keyup', (event) => { 
+	if ((event.code == "ShiftRight" || event.code == "ShiftLeft") && Caps == 0){
+		changecaps(0);
+	} 
+	else if ((event.code == "ShiftRight" || event.code == "ShiftLeft") && Caps == 1) {
+		changecaps(1);
+	} else {}
+});
+document.addEventListener('keydown', (event) => {
+	// Get key codes
+	var name = event.key;
+	var code = event.code;
+	
+	// Check for CapsLock
+	if (event.getModifierState('CapsLock')) {
+        changecaps(1);
+		Caps = 1;
+		if (event.getModifierState('Shift')){
+			changecaps(0);
+		}
+    } 
+	else if (event.getModifierState('Shift')){
+		changecaps(1);
+		Caps = 0;
+	}
+	else {
+        changecaps(0);
+		Caps = 0;
+    }
+	
+	// Highlight and input key
 	if (characters.includes(name) == true)
 	{
+		document.getElementById(name.toLowerCase()).style.backgroundColor = "#6dff5f";
+		setTimeout(clearcolors, 400, name.toLowerCase());
 		try{clearTimeout(g);}catch{}
 		inputfield.push(name)
 		document.getElementById("poletekst").innerHTML = inputfield.join("");
 		g = setTimeout(check, 1000);
-	}else{}
+	}
+	else if (characterscodes.includes(code) == true){
+		document.getElementById(code).style.backgroundColor = "#6dff5f";
+		setTimeout(clearcolors, 400, code);
+	} else{}
 }, false);
+
 function clearcolors(idname){
-	document.getElementById(idname).style.backgroundColor = "white";
+	document.getElementById(idname).style.backgroundColor = document.getElementsByClassName("keys")[0].style.backgroundColor;
 }
 function check(){
 	socket.emit('point', 1);
 	inputfield = [];
 }
-function ja()
-{
-	socket.emit('point', 1);
-}
 socket.on('poletekst', function(point) {
    document.getElementById("poletekst").innerHTML = point;
 });
-//letters.push(characters.charAt(Math.floor(Math.random() * characters.length)));
