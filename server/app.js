24d1bbb5bdd6c69b7d4f9a19cc8e8d9d5bb46ffd
cynka02 <33747300+cynka02@users.noreply.github.com>
2022-01-03 20:44:46 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -7,7 +7,6 @@ const publicPath = path.join(__dirname, '../client');
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 const io = socketIO(server);
-var Text = [];
 app.use(express.static(publicPath));
 server.listen(port);
 
@@ -15,21 +14,11 @@ server.listen(port);
 io.on('connection', (socket) => {
 	console.log('a user connected');
 	io.emit('poletekst', "...");
-	io.emit('chat', Text.join(""));
 	socket.on('disconnect', () => {
 		console.log('user disconnected');
 	});
-});
-
-io.on('connection', (socket) => {
-	socket.on('message', (co) => {
-		Text.push(co);
-		Text.push("<br />")
-		if (Text.length > 14){
-			Text.shift();
-			Text.shift();
-		}
-		io.emit('poletekst', "...");
-		io.emit('chat', Text.join(""));
+	
+	socket.on('LogPlayer', (PlayerData) => {
+			console.log("a");
 	});
 });
