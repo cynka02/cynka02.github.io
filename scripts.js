de3e0b1bd1d1a153f4f66e344f38742f8b25692b
cynka02 <33747300+cynka02@users.noreply.github.com>
2023-09-17 18:17:43 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -17,45 +17,31 @@ function load(){
         let lang = JSON.parse(localStorage.getItem('lang'));
         if (lang){ // polski
             document.getElementById("menu5").innerHTML ='<a href="#contact">Kontakt</a>';
-            document.getElementById("menu4").innerHTML ='<a href="#us">O nas</a>';
-            document.getElementById("menu3").innerHTML ='<a href="#career">Kariera</a>';
-            document.getElementById("menu2").innerHTML ='<a href="#services">Oferta</a>';
+            document.getElementById("menu4").innerHTML ='<a href="#info3">O nas</a>';
+            document.getElementById("menu3").innerHTML ='<a href="#info2">Kariera</a>';
+            document.getElementById("menu2").innerHTML ='<a href="#info1">Oferta</a>';
             document.getElementById("home").innerHTML ='Innowacyjne metody<br>obróbki danych';
-            document.getElementById("info1_header").innerHTML ='Slogan1';
-            document.getElementById("info1_text").innerHTML ='Tutaj powinny znaleźć się informacje o firmie, takie ogólne i chwytliwe.<br>Można się trochę rozpisać, nie ma ograniczeń<br><br><br>Symetrię napisów trzeba zmieniać ręcznie. Najpierw tekst, później pozycjonowanie.';
-            document.getElementById("info2_header").innerHTML ='Slogan2';
+            document.getElementById("info1_header").innerHTML ='Oferta';
+            document.getElementById("info1_text").innerHTML ='Tutaj powinny znaleźć się informacje o firmie i ofercie.<br><br><br>(Symetrię napisów trzeba zmieniać ręcznie)';
+            document.getElementById("info1_click").innerHTML ='<a href="offer.html">Więcej</a>';
+            document.getElementById("info2_header").innerHTML ='The LEGO Group';
             document.getElementById("info2_text").innerHTML ="Kolejne miejsce żeby coś opisać, dobrze się zaprezentować.<br>Można wspomnieć o współpracy z LEGO i poświecić temu to miejsce.";
-            document.getElementById("info3_header").innerHTML ='Slogan3';
-            document.getElementById("info3_text").innerHTML ='Można ostatecznie zachęcić przyszłych klientów do nas w tym akapicie.<br>Opisać kim jesteśmy, co cenimy lub jaką pracę wykonujemy.';
+            document.getElementById("info2_click").innerHTML ='<a href="partners.html">Więcej</a>';
+            document.getElementById("info3_header").innerHTML ='O nas';
+            document.getElementById("info3_text").innerHTML ='Można ostatecznie zachęcić przyszłych klientów do nas w tym akapicie.<br>Opisać kim jesteśmy, co cenimy w pracy itp.';
+            document.getElementById("info3_click").innerHTML ='<a href="us.html">Więcej</a>';
             document.getElementById("contact1header").innerHTML ='Adres';
             document.getElementById("contact1text").innerHTML ='Gdański Park Naukowo-Technologiczny<br>Trzy Lipy 3, 80-172 Gdańsk';
             document.getElementById("contact2header").innerHTML ='Telefon';
-            document.getElementById("contact2text").innerHTML ='+48 987 123 654<br>&nbsp;';
+            document.getElementById("contact2text").innerHTML ='<a href="tel:666223268">+48 987 123 654</a><br><b>Krzysztof</b>';
             document.getElementById("contact3header").innerHTML ='Mail';
-            document.getElementById("contact3text").innerHTML ='konrad.hennig@consultant.lego.com<br>&nbsp;';
+            document.getElementById("contact3text").innerHTML ='<a href="mailto:konrad.hennig@consultant.lego.com">konrad.hennig@consultant.lego.com</a><br>&nbsp;';
             document.getElementById("lang_pl").style = "cursor: auto;";
             document.getElementById("lang_en").style = "cursor: pointer;";
 
         }
     }
     else{ // angielski
-        document.getElementById("menu5").innerHTML ='<a href="#contact">Contact</a>';
-        document.getElementById("menu4").innerHTML ='<a href="#us">About us</a>';
-        document.getElementById("menu3").innerHTML ='<a href="#career">Careers</a>';
-        document.getElementById("menu2").innerHTML ='<a href="#services">Offer</a>';
-        document.getElementById("home").innerHTML ='Innovative solutions<br>in business analysis';
-        document.getElementById("info1_header").innerHTML ='Slogan1';
-        document.getElementById("info1_text").innerHTML ='Here will be catchy text about our company in general<br>Text can be even longer<br><br><br>The symmetry has to be adjusted manually';
-        document.getElementById("info2_header").innerHTML ='Slogan2';
-        document.getElementById("info2_text").innerHTML ="Here will be another text, to present our company.<br>We can mention cooperation with LEGO and make this place only for it";
-        document.getElementById("info3_header").innerHTML ='Slogan3';
-        document.getElementById("info3_text").innerHTML ='We can encourage customers to our company again.<br>Describe who we are, what we value the most and what kind of job we offer.';
-        document.getElementById("contact1header").innerHTML ='Address';
-        document.getElementById("contact1text").innerHTML ='Gdański Park Naukowo-Technologiczny<br>Trzy Lipy 3, 80-172 Gdańsk';
-        document.getElementById("contact2header").innerHTML ='Phone';
-        document.getElementById("contact2text").innerHTML ='+48 987 123 654<br>&nbsp;';
-        document.getElementById("contact3header").innerHTML ='Mail';
-        document.getElementById("contact3text").innerHTML ='konrad.hennig@consultant.lego.com<br>&nbsp;';
         document.getElementById("lang_en").style = "cursor: auto;";
         document.getElementById("lang_pl").style = "cursor: pointer;";
     }
@@ -86,7 +72,7 @@ function reveal() {
     for (var i = 0; i < reveals2.length; i++) {
       var windowHeight = window.innerHeight;
       var elementTop = reveals2[i].getBoundingClientRect().top;
-      var elementVisible = 70;
+      var elementVisible = 55;
       if (elementTop < windowHeight - elementVisible) {
         reveals2[i].classList.add("active");
       }
